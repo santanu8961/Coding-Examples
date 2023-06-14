2fe4b7b6e0ce4320f3b1e2530a6b1ec58ccc8ba7
santanu8961 <43335319+santanu8961@users.noreply.github.com>
2023-06-15 03:37:30 +0530

    Debounce Example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
+  const [value, setValue] = React.useState('');
+  const [inp, setinp] = React.useState('');
+  const delayConfigureFn = (cb, delay) => {
+    let timer;
+    clearInterval(timer);
+    return (...args) => {
+      timer = setTimeout(() => {
+        cb(...args);
+      }, delay);
+    };
+  };
+  const debounceFn = delayConfigureFn((e) => {
+    setValue(e.target.value);
+  }, 500);
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <p>Example of Event Debounce</p>
+      <input
+        type="value"
+        // value={inp}
+        onKeyUp={(e) => {
+          debounceFn(e);
+        }}
+      />
+      <p>Debounce Text: {value}</p>
     </div>
   );
 }
