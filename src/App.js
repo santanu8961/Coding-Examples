b1c21f79a572d407624f4fe4e3d2a13c22cf9e58
santanu8961 <43335319+santanu8961@users.noreply.github.com>
2023-06-16 21:16:59 +0530

    1 file modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './style.css';
-import { ImportantJsConcepts } from './important-js-concepts';
+import { DigitalWatch } from './digital-watch';
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
-      <ImportantJsConcepts />
+      <DigitalWatch />
     </div>
   );
 }
